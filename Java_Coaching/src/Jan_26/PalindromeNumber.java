1594735b8506dfd1a8c1d1c61511e33b87e38f88
Sadia Shah <Sadia Shah@192.168.1.204>
2019-01-30 22:04:05 +0530

    Palindrome Code

--- Java_Coaching/src/Jan_26/PalindromeNumber.java
+++ Java_Coaching/src/Jan_26/PalindromeNumber.java
@@ -5,24 +5,40 @@ import java.util.Scanner;
 public class PalindromeNumber {
 	
 	public static void main(String[] args) {
+		int r;
+		int sum=0;
 		Scanner s = new Scanner(System.in);
 		System.out.println("Please enter minimum two digit number");
 		int num = s.nextInt();
 		System.out.println("Enter Number is :- "+num);
-		int a= num%10;
-		int b = num/10;
-		int c= b%10;
-		int d= b/10;
-		int e = (a*100 ) +(c*10) +d;
-		if(num==e)
+		int temp=num;
+		while(num>0)
+		{
+			r=num%10;
+			sum = (sum*10) +r;
+			num=num/10;
+			
+		}
+		System.out.println(sum);
+		System.out.println(temp);
+		if(temp==sum)
 		{
 			System.out.println("Palindrome Number");
 		}
 		else
 		{
-			System.out.println("Not Palindrome Number");
+			System.out.println("Not a Palindrome Number");
 		}
 		
+		/*
+		 * class PalindromeExample{ public static void main(String args[]){ int
+		 * r,sum=0,temp; int n=454;//It is the number variable to be checked for
+		 * palindrome
+		 * 
+		 * temp=n; while(n>0){ r=n%10; //getting remainder sum=(sum*10)+r; n=n/10; }
+		 * if(temp==sum) System.out.println("palindrome number "); else
+		 * System.out.println("not palindrome"); } }
+		 */
 		
 	}
 
