de4da0f446ecbfc784661200fb31772662d731be
Sadia Shah <Sadia Shah@192.168.1.204>
2019-01-30 23:19:01 +0530

    Fibonacci Series

---
+++ Java_Coaching/src/Jan_26/FibonacciSeries.java
@@ -0,0 +1,26 @@
+package Jan_26;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+	
+	public static void main(String[] args) {
+		int a=0;
+		int b=1;
+		int sum=0;
+		System.out.println("Please Enter Number");
+		Scanner s = new Scanner(System.in);
+		int num = s.nextInt();
+		
+		for(int i=1;i<=num;i++) {
+			
+			sum = a+b;
+			System.out.println(a + "");
+			a=b;
+			
+			b=sum;
+		}
+		
+	}
+
+}
