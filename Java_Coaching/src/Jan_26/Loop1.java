51f547906febf62665baaccd0a452f609634a1a1
Sadia Shah <Sadia Shah@192.168.1.204>
2019-02-01 00:15:50 +0530

    First Loop Program

---
+++ Java_Coaching/src/Jan_26/Loop1.java
@@ -0,0 +1,26 @@
+package Jan_26;
+/*
+ 
+*
+**
+***
+****
+*****
+
+*/
+public class Loop1 {
+	
+	public static void main(String[] args) {
+		
+		for(int row=1;row<=5;row++) {
+			
+			for(int col =1;col<=row;col++)
+			{
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+		
+	}
+
+}
