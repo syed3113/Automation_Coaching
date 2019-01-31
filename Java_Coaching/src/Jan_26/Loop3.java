590db1637ccf3b70b6448d2263987150402d90c0
Sadia Shah <Sadia Shah@192.168.1.204>
2019-02-01 00:37:51 +0530

    Loop 3 Program

---
+++ Java_Coaching/src/Jan_26/Loop3.java
@@ -0,0 +1,22 @@
+package Jan_26;
+
+public class Loop3 {
+/*
+ 1 
+12 
+123 
+1234 
+12345 
+ */
+		
+ public static void main(String[] args) {
+	 for(int row=1; row<=5;row++)
+	 {
+		 for(int col=1; col<=row;col++)
+		 {
+			 System.out.print(col);
+		 }
+		 System.out.println(" ");
+	 }
+}
+}
