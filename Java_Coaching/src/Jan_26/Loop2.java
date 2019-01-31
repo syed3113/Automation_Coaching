235000dcaaced41ebd33ad6d7698b2eb3e66bbf3
Sadia Shah <Sadia Shah@192.168.1.204>
2019-02-01 00:32:25 +0530

    Loop 2 program

---
+++ Java_Coaching/src/Jan_26/Loop2.java
@@ -0,0 +1,17 @@
+package Jan_26;
+
+public class Loop2 {
+	
+	public static void main(String[] args) {
+		
+		for(int row=1;row<=5;row++) {
+			
+			for(int col=5;col>=row;col--)
+			{
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+	}
+
+}
