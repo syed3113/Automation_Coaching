e5e9b82815e2c5b8c076eb8598e65b19732d54d6
Sadia Shah <Sadia Shah@192.168.1.204>
2019-02-03 00:26:28 +0530

    Read Write

---
+++ Java_Coaching/src/Feb_2/ReadWriteFile.java
@@ -0,0 +1,50 @@
+package Feb_2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ReadWriteFile {
+	
+	
+	public String data_read(String path) throws IOException
+	{
+		File f = new File(path);
+		FileReader fr = new FileReader(f);
+		BufferedReader br = new BufferedReader(fr);
+		String s = br.readLine();
+		return s;
+		
+		
+	}
+	public void data_write(String path,String val) throws IOException
+	{
+		
+		File f = new File(path);
+		FileWriter fw = new FileWriter(f);
+		BufferedWriter br = new BufferedWriter(fw);
+		br.write(val);
+		br.close();
+		
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) throws IOException {
+		
+		ReadWriteFile wf = new ReadWriteFile();
+		String value = wf.data_read("C:\\Users\\Sadia Shah\\Desktop\\outFile.txt");
+		System.out.println("Value:-"+value);
+		ReadWriteFile wf1 = new ReadWriteFile();
+		wf1.data_write("C:\\Users\\Sadia Shah\\Desktop\\readWrite.txt",value);
+		
+		
+	}
+  
+}
