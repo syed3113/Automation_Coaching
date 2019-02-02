cd9f601ba5b50502e0e69cbd04a421228c3a9dad
Sadia Shah <Sadia Shah@192.168.1.204>
2019-02-02 20:20:09 +0530

    Reading By Line number

---
+++ Java_Coaching/src/Feb_2/ReadingByLineNumber.java
@@ -0,0 +1,39 @@
+package Feb_2;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadingByLineNumber {
+
+	public String read_data(String path, int lineNo) throws IOException
+	{
+		File f = new File(path);
+		FileReader fr = new FileReader(f);
+		BufferedReader br = new BufferedReader(fr);
+		int i=0;
+		String s;
+		
+		while((s=br.readLine())!=null) {
+			
+			i=i+1;
+			if(i==lineNo) {
+				
+				break;
+			}
+				
+		}
+		return s;
+	}
+
+	public static void main(String[] args) throws IOException {
+		
+		ReadingByLineNumber rl = new ReadingByLineNumber();
+		String value =rl.read_data("C:\\Users\\Sadia Shah\\Desktop\\outFile.txt", 3);
+		System.out.println("Value is" +value);
+		
+	}
+	
+}
