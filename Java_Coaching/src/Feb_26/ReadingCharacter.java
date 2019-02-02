63df7da998a12dcb1b830525423ac1879656298d
Sadia Shah <Sadia Shah@192.168.1.204>
2019-02-02 17:40:28 +0530

    Reading character from txt file

---
+++ Java_Coaching/src/Feb_26/ReadingCharacter.java
@@ -0,0 +1,26 @@
+package Feb_26;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+// Reading character by character from text file
+public class ReadingCharacter {
+	
+	public static void main(String[] args) throws IOException {
+		 
+		File f = new File("C:\\Users\\Sadia Shah\\Desktop\\inputFile.txt");
+		
+		FileReader fr = new FileReader(f); // FileReader class is used for reading character by character from txt file 
+		int r;
+		while((r=fr.read())!=-1) //storing in r because return type is int & comparing with -1 because there is no ASCII value equal -1
+		{
+			System.out.println((char)r);
+		}
+			
+			
+	}
+	
+
+}
